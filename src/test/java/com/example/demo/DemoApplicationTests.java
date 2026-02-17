f9f3bc3e2324f0859e2f40cbf0ea6787dd282666
Leon <iiiedujava@gmail.com>
2026-02-18 00:07:45 +0800

    add test properties file and enable test again

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class DemoApplicationTests {
 
 	@Test
