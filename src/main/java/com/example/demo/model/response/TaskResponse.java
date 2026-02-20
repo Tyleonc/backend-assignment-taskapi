39a5076801756bcd749339c586cb3e93b3910852
Leon <iiiedujava@gmail.com>
2026-02-20 13:48:32 +0800

    remove bean validation to avoid semantic misunderstanding

--- src/main/java/com/example/demo/model/response/TaskResponse.java
+++ src/main/java/com/example/demo/model/response/TaskResponse.java
@@ -1,17 +1,12 @@
 package com.example.demo.model.response;
 
 import com.example.demo.model.Payload;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.time.Instant;
 
 public record TaskResponse(
-        @NotBlank
         String taskId,
-        @NotNull
         String status,
-        @NotNull
         Payload payload,
         Instant executeAt
 ) {}
