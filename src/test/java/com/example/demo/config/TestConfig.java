d131c5d4cf5df06968170f52aa7febaba9b4e6c0
Leon <iiiedujava@gmail.com>
2026-02-21 18:42:23 +0800

    rename test configuration class and add mock for mq producer

--- src/test/java/com/example/demo/config/TestConfig.java
+++ src/test/java/com/example/demo/config/TestConfig.java
@@ -1,15 +1,22 @@
 package com.example.demo.config;
 
 import com.example.demo.service.TaskService;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
-public class WebMvcTestConfig {
+public class TestConfig {
 
     @Bean
     public TaskService taskService() {
         return Mockito.mock(TaskService.class);
     }
+
+    @Bean
+    public DefaultMQProducer mockDefaultMQProducer() {
+        return Mockito.mock(DefaultMQProducer.class);
+    }
+
 }
