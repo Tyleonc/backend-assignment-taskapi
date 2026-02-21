5486974b2ca98294010cd333f51e1d730f08fd9f
Leon <iiiedujava@gmail.com>
2026-02-21 19:56:03 +0800

    add new repo method to fetch single task data

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -4,8 +4,12 @@ import com.example.demo.entity.ScheduledTaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<ScheduledTaskEntity, Long> {
 
     boolean existsByTaskId(String taskId);
+
+    Optional<ScheduledTaskEntity> findByTaskId(String taskId);
 }
