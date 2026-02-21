9c3361dc13ff8348a994287cbc0efee4c9c1b3ad
Leon <iiiedujava@gmail.com>
2026-02-21 20:38:16 +0800

    remove unnecessary annotation

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -2,11 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ScheduledTaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface TaskRepository extends JpaRepository<ScheduledTaskEntity, Long> {
 
     boolean existsByTaskId(String taskId);
