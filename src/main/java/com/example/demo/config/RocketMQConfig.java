102db79a8147f9873e62b38d56c0d412a4132582
Leon <iiiedujava@gmail.com>
2026-02-22 12:19:21 +0800

    add log for initialization of mq producer

--- src/main/java/com/example/demo/config/RocketMQConfig.java
+++ src/main/java/com/example/demo/config/RocketMQConfig.java
@@ -1,21 +1,34 @@
 package com.example.demo.config;
 
 import com.example.demo.config.property.RocketMQProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
 @EnableConfigurationProperties(RocketMQProperties.class)
+@Slf4j
 public class RocketMQConfig {
 
     @Bean(destroyMethod = "shutdown")
     public DefaultMQProducer defaultMQProducer(RocketMQProperties properties) throws MQClientException {
-        DefaultMQProducer producer = new DefaultMQProducer(properties.getProducer().getGroup());
-        producer.setNamesrvAddr(properties.getNamesrvAddr());
+        String group = Optional.ofNullable(properties.getProducer())
+                .map(RocketMQProperties.ProducerProperties::getGroup)
+                .orElse(null);
+
+        String nameSrvAddr = properties.getNamesrvAddr();
+        DefaultMQProducer producer = new DefaultMQProducer(group);
+        producer.setNamesrvAddr(nameSrvAddr);
         producer.start();
+
+        log.info("RocketMQ Producer initialized: group={}, instance={}, namesrv={}",
+                group, producer.getInstanceName(), nameSrvAddr);
+
         return producer;
     }
 
