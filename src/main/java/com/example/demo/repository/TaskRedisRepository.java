dfaa5b863f0096ea604f3b750a1ec828f41b13cb
Leon <iiiedujava@gmail.com>
2026-02-22 19:51:03 +0800

    add new method in redis repo

--- src/main/java/com/example/demo/repository/TaskRedisRepository.java
+++ src/main/java/com/example/demo/repository/TaskRedisRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
+import java.util.Set;
 
 @Repository
 public class TaskRedisRepository {
@@ -18,6 +20,15 @@ public class TaskRedisRepository {
 
     public void schedule(String taskId, Instant executeAt) {
         redisTemplate.opsForZSet().add(DELAY_QUEUE_KEY, taskId, executeAt.toEpochMilli());
+    }
+
+    public Set<String> getDueTaskBatch(Instant now, long batch) {
+        ZSetOperations<String, String> ops = redisTemplate.opsForZSet();
+        return ops.rangeByScore(DELAY_QUEUE_KEY, 0D, now.toEpochMilli(), 0L, batch);
+    }
+
+    public void removeTask(String taskId) {
+        redisTemplate.opsForZSet().remove(DELAY_QUEUE_KEY, taskId);
     }
 
 }
