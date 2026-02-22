f11a758e9326cd8b08ad7d96b1405e62c49f41d1
Leon <iiiedujava@gmail.com>
2026-02-23 01:15:11 +0800

    introduce event type for listener to decide what to trigger

---
+++ src/main/java/com/example/demo/model/event/EventType.java
@@ -0,0 +1,6 @@
+package com.example.demo.model.event;
+
+public enum EventType {
+    CREATE,
+    CANCEL
+}
--- src/main/java/com/example/demo/model/event/TaskEvent.java
+++ src/main/java/com/example/demo/model/event/TaskEvent.java
@@ -2,7 +2,8 @@ package com.example.demo.model.event;
 
 import java.time.Instant;
 
-public record CreateTaskEvent(
+public record TaskEvent(
+        EventType type,
         String taskId,
         Instant executeAt
 ) {}
