7af9b5f7a9868feccdabd681f9968e030704c213
Leon <iiiedujava@gmail.com>
2026-02-23 01:16:35 +0800

    handle cancel task

--- src/main/java/com/example/demo/service/CreateTaskEventListener.java
+++ src/main/java/com/example/demo/service/CreateTaskEventListener.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
-import com.example.demo.model.event.CreateTaskEvent;
+import com.example.demo.model.event.TaskEvent;
 import com.example.demo.repository.TaskRedisRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.time.Instant;
 
 @Component
 @Slf4j
@@ -18,8 +20,18 @@ public class CreateTaskEventListener {
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleCreateTaskEvent(CreateTaskEvent event) {
-        taskRedisRepository.schedule(event.taskId(), event.executeAt());
-        log.info("task {} is scheduled at {}", event.taskId(), event.executeAt());
+    public void handleTaskEvent(TaskEvent event) {
+
+        switch (event.type()) {
+            case CREATE -> {
+                taskRedisRepository.schedule(event.taskId(), event.executeAt());
+                log.info("task {} is scheduled at {}", event.taskId(), event.executeAt());
+            }
+
+            case CANCEL -> {
+                taskRedisRepository.removeSingleTask(event.taskId());
+                log.info("task {} is cancelled at {}", event.taskId(), Instant.now());
+            }
+        }
     }
 }
